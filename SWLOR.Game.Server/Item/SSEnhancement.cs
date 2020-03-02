e53e7b254c2bdff65f560eb970c0032c9199d411
Zunath <coolty3001@yahoo.com>
2020-03-01 19:33:40 -0500

    Clean up usings on SSEnhancement.cs

--- SWLOR.Game.Server/Item/SSEnhancement.cs
+++ SWLOR.Game.Server/Item/SSEnhancement.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using NWN;
 using SWLOR.Game.Server.Data.Entity;
-using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.GameObject;
 using SWLOR.Game.Server.Item.Contracts;
-
 using SWLOR.Game.Server.ValueObject;
-using System.Linq;
 using SWLOR.Game.Server.NWScript;
 using SWLOR.Game.Server.NWScript.Enumerations;
 using SWLOR.Game.Server.Service;
-using static SWLOR.Game.Server.NWScript._;
 
 namespace SWLOR.Game.Server.Item
 {
